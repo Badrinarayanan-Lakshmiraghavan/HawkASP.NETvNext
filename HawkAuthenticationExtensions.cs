4daf080577a3ab2f23884f2f342a7ecf23b5951a
Badri Lakshmiraghavan <badri.lr@gmail.com>
2014-10-30 09:08:38 +0530

    Cleanup

--- HawkAuthenticationExtensions.cs
+++ HawkAuthenticationExtensions.cs
@@ -6,11 +6,6 @@ namespace HawkvNext
 {
     public static class HawkAuthenticationExtensions
     {
-        //public static IServiceCollection ConfigureCookieAuthentication([NotNull] this IServiceCollection services, [NotNull] Action<CookieAuthenticationOptions> configure)
-        //{
-        //    return services.Configure(configure);
-        //}
-
         public static IApplicationBuilder UseHawkAuthentication(this IApplicationBuilder app, Action<HawkAuthenticationOptions> configureOptions = null, string optionsName = "")
         {
             return app.UseMiddleware<HawkAuthenticationMiddleware>(
